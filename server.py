7c12ac127f34dd54c4b3989a616b07e0e8efc8c7
Timothy Stander <timothystander@gmail.com>
2024-08-17 15:51:47 -0600

    refactor

--- server.py
+++ server.py
@@ -9,7 +9,7 @@ PORT = 8000  # You can choose any port number
 
 class SimpleHandler(http.server.SimpleHTTPRequestHandler):
     light_process = subprocess.Popen(['/home/tstander/python/bin/python', 'light.py'],stdin=subprocess.PIPE, text=True)
-    def finish(self, speak="", keep_listening=False,context={}):
+    def say(self, speak="", keep_listening=False,context={}):
         self.send_response(200)
         json_data = json.dumps({"speak": speak,"await":keep_listening,"context":context})
         self.send_header("Content-type", "application/json")
@@ -29,9 +29,9 @@ class SimpleHandler(http.server.SimpleHTTPRequestHandler):
             try:
                 commands = json.loads(post_data)
             except:
-                return self.finish()
+                return self.illegal_request("Invalid JSON")
             if not isinstance(commands, list):
-                return self.illegal_request("Invalid request A")
+                return self.illegal_request("Unexpected structure")
             jarvis=False
             for command in commands:
                 command=command.lower()
@@ -41,22 +41,23 @@ class SimpleHandler(http.server.SimpleHTTPRequestHandler):
                 if "light" in command and "turn" in command and "on" in command:
                     self.light_process.stdin.write("on\n")
                     self.light_process.stdin.flush()
-                    return self.finish(speak="Turning on light")
+                    return self.say(speak="Turning on light")
                 elif "light" in command and "turn" in command and "off" in command:
                     self.light_process.stdin.write("off\n")
                     self.light_process.stdin.flush()
-                    return self.finish(speak="Turning off light")
+                    return self.say(speak="Turning off light")
                 elif "light" in command and "brightness" in command:
                     # Send a 200 OK response
-                    brightness = re.findall(r'\d+',command)
-                    if len(brightness) == 0:
-                        return self.finish()
+                    numbers = re.findall(r'\d+',command)
+                    if len(numbers) == 0:
+                        return self.say()
+                    brightness = numbers[0]
                     self.light_process.stdin.write("brightness " + brightness + "\n")
                     self.light_process.stdin.flush()
-                    return self.finish(speak="Setting brightness to " + brightness + " percent")
+                    return self.say(speak="Setting brightness to " + brightness + " percent")
             if not jarvis:
-                return self.finish()
-            return self.finish(speak="Javis doesn't know how to "+command[0])
+                return self.say()
+            return self.say(speak="Javis doesn't know how to "+command[0])
         else:
             # Handle other paths with a 404 Not Found
             self.send_response(404)
