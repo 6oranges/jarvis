7b7636a902a91774faa2982bdb6d8b8dc7b610f8
Timothy Stander <timothystander@gmail.com>
2024-08-17 00:51:03 -0600

    add brightness controls

--- server.py
+++ server.py
@@ -2,6 +2,7 @@ import http.server
 import socketserver
 import asyncio
 import json
+import re
 from kasa import Discover
 
 PORT = 8000  # You can choose any port number
@@ -10,12 +11,16 @@ async def get_device():
     global ip_address
     device =  await Discover.discover_single(ip_address)
     if device is not None:
-        print(f"Found device: {device}")
+        print(f"Using device at IP: {ip_address} with alias: {device.alias}")
+        print(f"Using existing device: {device}")
         return device
     devices = await Discover.discover()
     for key in devices:
         ip_address = key
-        print(f"Found device: {device}")
+        if "tim" not in devices[key].alias.lower():
+            continue
+        print(f"Found device at IP: {ip_address} with alias: {devices[key].alias}")
+        print(f"Using device: {device}")
         return devices[key]
 async def turn_on_device():
     dev = await get_device()
@@ -28,7 +33,12 @@ async def turn_off_device():
     await dev.turn_off()
     await dev.update()
     print("Device turned off")
-
+async def set_brightness(brightness):
+    dev = await get_device()
+    # Probably a bug in kasa, but we need to use _set_brightness instead of set_brightness
+    await dev._set_brightness(brightness)
+    await dev.update()
+    print("Device brightness set to", brightness)
 class SimpleHandler(http.server.SimpleHTTPRequestHandler):
     def do_POST(self):
         # Start an asyncio event loop to handle async functions
@@ -42,14 +52,14 @@ class SimpleHandler(http.server.SimpleHTTPRequestHandler):
                 commands = json.loads(post_data)
             except:
                 self.send_response(200)
-                self.send_header("Content-type", "text/html")
+                self.send_header("Content-type", "text/plain")
                 self.end_headers()
                 # Sent no data
                 self.wfile.write(b"")
                 return
             if not isinstance(commands, list):
                 self.send_response(400)
-                self.send_header("Content-type", "text/html")
+                self.send_header("Content-type", "text/plain")
                 self.end_headers()
                 self.wfile.write(b"Invalid request B")
                 return
@@ -60,7 +70,7 @@ class SimpleHandler(http.server.SimpleHTTPRequestHandler):
                 if "light" in command and "turn" in command and "on" in command:
                     # Send a 200 OK response
                     self.send_response(200)
-                    self.send_header("Content-type", "text/html")
+                    self.send_header("Content-type", "text/plain")
                     self.end_headers()
                     self.wfile.write(b"Turning on light")
                     self.wfile.flush()
@@ -69,21 +79,31 @@ class SimpleHandler(http.server.SimpleHTTPRequestHandler):
                 elif "light" in command and "turn" in command and "off" in command:
                     # Send a 200 OK response
                     self.send_response(200)
-                    self.send_header("Content-type", "text/html")
+                    self.send_header("Content-type", "text/plain")
                     self.end_headers()
                     self.wfile.write(b"Turning off light")
                     self.wfile.flush()
                     loop.run_until_complete(turn_off_device())
                     return
+                elif "light" in command and "brightness" in command:
+                    # Send a 200 OK response
+                    brightness = int(re.findall(r'\d+',command)[0])
+                    self.send_response(200)
+                    self.send_header("Content-type", "text/plain")
+                    self.end_headers()
+                    self.wfile.write(bytes("Setting brightness to " + str(brightness) + " percent","utf-8"))
+                    self.wfile.flush()
+                    loop.run_until_complete(set_brightness(brightness))
+                    return
             # Handle other paths with a 404 Not Found
             self.send_response(200)
-            self.send_header("Content-type", "text/html")
+            self.send_header("Content-type", "text/plain")
             self.end_headers()
             self.wfile.write(b"")
         else:
             # Handle other paths with a 404 Not Found
             self.send_response(404)
-            self.send_header("Content-type", "text/html")
+            self.send_header("Content-type", "text/plain")
             self.end_headers()
             self.wfile.write(b"Not Found")
         
